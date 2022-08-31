71fdc456b57b022f264dde586ce0abf283a9e4fa
FusRoman <roman.lemontagner@gmail.com>
2022-08-31 08:19:28 +0000

    remove secret

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "fink_grb" : ["conf/fink_grb.conf"] 
     },
     install_requires=[
+        "docopt>=0.6.2",
         "fink-science>=0.5.1",
         "fink-utils>=0.3.0",
         "numpy>=1.17",
