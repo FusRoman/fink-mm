dc1ce999760f0c7f89522f9c685ce430f2a68041
FusRoman <roman.lemontagner@gmail.com>
2022-09-13 15:02:27 +0000

    add scheduler

--- scheduler/science2grb.sh
+++ scheduler/science2grb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
  
 source ~/.bash_profile
- 
+source /opt/anaconda/etc/profile.d/conda.sh
+
+
+conda deactivate
+conda activate test_env
+
+
 NIGHT=`date +"%Y%m%d" -d "now + 1 days"`
 YEAR=${NIGHT:0:4}
 MONTH=${NIGHT:4:2}
 DAY=${NIGHT:6:2}
  
-FINK_GRB_HOME = "/home/roman.le-montagner/Doctorat/GRB/Fink_GRB_test"
+FINK_GRB_HOME="/home/roman.le-montagner/Doctorat/GRB/Fink_GRB_test"
 
 while true; do
      $(hdfs dfs -test -d /user/julien.peloton/online/science/year=${YEAR}/month=${MONTH}/day=${DAY})
