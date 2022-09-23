fb497690bdf913905fb4cccb8bde99233d8cdf99
FusRoman <roman.lemontagner@gmail.com>
2022-09-23 14:12:29 +0000

    change the scheduler

--- scheduler/science2grb.sh
+++ scheduler/science2grb.sh
@@ -18,13 +18,16 @@ FINK_GRB_HOME="/home/roman.le-montagner/Doctorat/GRB/Fink_GRB_test"
 while true; do
      $(hdfs dfs -test -d /user/julien.peloton/online/science/year=${YEAR}/month=${MONTH}/day=${DAY})
      if [[ $? == 0 ]]; then
-         echo "Launching service"
- 
-         # LEASETIME must be computed by taking the difference between now and max end 
-         LEASETIME=$(( `date +'%s' -d '17:00 today'` - `date +'%s' -d 'now'` ))
- 
-         nohup fink_grb join_stream --config ${FINK_GRB_HOME}/local.conf --night ${NIGHT} --exit_after ${LEASETIME} > ${FINK_GRB_HOME}/join_stream.log &
-         exit
+        $(test -d gcn_storage/raw/year=${YEAR}/month=${MONTH}/day=${DAY})
+        if [[ $? == 0 ]]; then
+            echo "Launching service"
+    
+            # LEASETIME must be computed by taking the difference between now and max end 
+            LEASETIME=$(( `date +'%s' -d '17:00 today'` - `date +'%s' -d 'now'` ))
+    
+            nohup fink_grb join_stream --config ${FINK_GRB_HOME}/local.conf --night ${NIGHT} --exit_after ${LEASETIME} > ${FINK_GRB_HOME}/join_stream.log &
+            exit
+        fi
      fi
      DDATE=`date`
      echo "${DDATE}: no data yet. Sleeping..."
