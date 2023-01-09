fdde0b034926efe22602b0db5f8232d6ba18d952
FusRoman <roman.lemontagner@gmail.com>
2023-01-09 11:01:39 +0000

    test

--- fink_grb/offline/spark_offline.py
+++ fink_grb/offline/spark_offline.py
@@ -133,10 +133,9 @@ def spark_offline(
     ... )
 
     >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet")
+    >>> datatest = datatest[datatest.columns[:17] + datatest.columns[-3:]]
     >>> datajoin = pd.read_parquet(grb_dataoutput + "/year=2019")
 
-    >>> print(datatest.columns)
-    >>> print(datajoin.columns)
     >>> assert_frame_equal(datatest, datajoin, check_dtype=False, check_column_type=False, check_categorical=False)
 
     >>> shutil.rmtree(grb_dataoutput + "/year=2019")
