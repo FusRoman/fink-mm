dd9783ba1cf9326d4acec6b9662959ffa10f48d4
FusRoman <roman.lemontagner@gmail.com>
2023-01-09 13:24:10 +0000

    test

--- fink_grb/offline/spark_offline.py
+++ fink_grb/offline/spark_offline.py
@@ -133,9 +133,9 @@ def spark_offline(
     ... )
 
     >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet").sort_values(["objectId", "triggerId"]).reset_index(drop=True)
-    >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime", "grb_proba"], axis=1)
+    >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime"], axis=1)
     >>> datajoin = pd.read_parquet(grb_dataoutput + "/year=2019").sort_values(["objectId", "triggerId"]).reset_index(drop=True)
-    >>> datajoin = datajoin.drop("grb_proba", axis=1)
+    # >>> datajoin = datajoin.drop("grb_proba", axis=1)
 
 
     >>> print(datatest[["objectId", "triggerId"]])
