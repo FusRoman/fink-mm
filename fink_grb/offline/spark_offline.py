287f0168943281f1d2b568e12b8adaab2140b06a
FusRoman <roman.lemontagner@gmail.com>
2023-01-10 15:56:01 +0000

    test spark_offline command line

--- fink_grb/offline/spark_offline.py
+++ fink_grb/offline/spark_offline.py
@@ -393,7 +393,7 @@ def launch_offline_mode(arguments, is_test=False):
         "spark_offline.py prod",
     )
 
-    start_window_in_jd = Time(parser.parse(night), format="datetime").jd
+    start_window_in_jd = Time(parser.parse(night), format="datetime").jd + 0.49
 
     application += " " + hbase_catalog
     application += " " + gcn_datapath_prefix
