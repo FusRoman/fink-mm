d2a8204268fcd56c91d69d013f4414738f09d4da
FusRoman <roman.lemontagner@gmail.com>
2023-01-10 14:00:40 +0000

    test spark_offline command line

--- fink_grb/offline/spark_offline.py
+++ fink_grb/offline/spark_offline.py
@@ -302,24 +302,24 @@ def launch_offline_mode(arguments, is_test=False):
 
     Examples
     --------
-    # >>> gcn_datatest = "fink_grb/test/test_data/gcn_test"
-    # >>> grb_dataoutput = "fink_grb/test/test_output"
-    # >>> launch_offline_mode({
-    # ... "--config" : None,
-    # ... "--night" : "20190903",
-    # ... "--exit_after" : 90
-    # ... },
-    # ... is_test=True)
+    >>> gcn_datatest = "fink_grb/test/test_data/gcn_test"
+    >>> grb_dataoutput = "fink_grb/test/test_output"
+    >>> launch_offline_mode({
+    ... "--config" : None,
+    ... "--night" : "20190903",
+    ... "--exit_after" : 90
+    ... },
+    ... is_test=True)
 
-    # >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
-    # >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime", "grb_proba"], axis=1)
-    # >>> datajoin = pd.read_parquet(grb_dataoutput + "/year=2019").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
-    # >>> datajoin = datajoin.drop("grb_proba", axis=1)
+    >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
+    >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime", "grb_proba"], axis=1)
+    >>> datajoin = pd.read_parquet(grb_dataoutput + "/year=2019").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
+    >>> datajoin = datajoin.drop("grb_proba", axis=1)
 
-    # >>> assert_frame_equal(datatest, datajoin, check_dtype=False, check_column_type=False, check_categorical=False)
+    >>> assert_frame_equal(datatest, datajoin, check_dtype=False, check_column_type=False, check_categorical=False)
 
-    # >>> shutil.rmtree(grb_dataoutput + "/year=2019")
-    # >>> os.remove(grb_dataoutput + "/_SUCCESS")
+    >>> shutil.rmtree(grb_dataoutput + "/year=2019")
+    >>> os.remove(grb_dataoutput + "/_SUCCESS")
     """
     config = get_config(arguments)
     logger = init_logging()
