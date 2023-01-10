edf59af2d728dbf94eb6502d210393aad32a5f7d
FusRoman <roman.lemontagner@gmail.com>
2023-01-10 08:32:20 +0000

    sort by objectId and triggerId in the online mode

--- fink_grb/online/ztf_join_gcn.py
+++ fink_grb/online/ztf_join_gcn.py
@@ -169,7 +169,7 @@ def ztf_join_gcn_stream(
     >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet")
     >>> datajoin = pd.read_parquet(grb_dataoutput + "/grb/year=2019")
 
-    >>> assert_frame_equal(datatest, datajoin, check_dtype=False, check_column_type=False, check_categorical=False)
+    >>> assert_frame_equal(datatest.sort_values(["objectId", "triggerId"]), datajoin.sort_values(["objectId", "triggerId"]), check_dtype=False, check_column_type=False, check_categorical=False)
 
     >>> shutil.rmtree(grb_dataoutput + "/grb/_spark_metadata")
     >>> shutil.rmtree(grb_dataoutput + "/grb/year=2019")
