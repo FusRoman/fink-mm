da27969aa29f610c3aa98e6bd68325646d3ab1d5
FusRoman <roman.lemontagner@gmail.com>
2023-02-10 07:59:27 +0000

    new test data file for offline mode

--- fink_grb/offline/spark_offline.py
+++ fink_grb/offline/spark_offline.py
@@ -167,10 +167,10 @@ def spark_offline(
     ... False
     ... )
 
-    >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
-    >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime", "grb_proba"], axis=1)
+    >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_offline_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
+    >>> datatest = datatest.drop(["grb_proba"], axis=1)
     >>> datajoin = pd.read_parquet(grb_dataoutput + "/offline/year=2019").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
-    >>> datajoin.to_parquet("fink_grb/test/test_data/grb_join_output.parquet")
+    >>> datajoin.to_parquet("fink_grb/test/test_data/grb_offline_join_output.parquet")
     >>> datajoin = datajoin.drop("grb_proba", axis=1)
 
     >>> assert_frame_equal(datatest, datajoin, check_dtype=False, check_column_type=False, check_categorical=False)
@@ -326,8 +326,8 @@ def launch_offline_mode(arguments, is_test=False):
     ... is_test=True
     ... )
 
-    >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
-    >>> datatest = datatest.drop(["delta_mag", "rate", "from_upper", "start_vartime", "diff_vartime", "grb_proba"], axis=1)
+    >>> datatest = pd.read_parquet("fink_grb/test/test_data/grb_offline_join_output.parquet").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
+    >>> datatest = datatest.drop(["grb_proba"], axis=1)
     >>> datajoin = pd.read_parquet(grb_dataoutput + "/year=2019").sort_values(["objectId", "triggerId", "grb_ra"]).reset_index(drop=True)
     >>> datajoin = datajoin.drop("grb_proba", axis=1)
 
