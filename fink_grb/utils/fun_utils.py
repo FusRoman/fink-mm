2ee34d56afdd6a39b9d353bef0e81d28263264db
FusRoman <roman.lemontagner@gmail.com>
2023-04-12 13:55:38 +0000

    fix test query pixels

--- fink_grb/utils/fun_utils.py
+++ fink_grb/utils/fun_utils.py
@@ -344,7 +344,7 @@ def get_pixels(rawEvent: pd.Series, NSIDE: pd.Series) -> pd.Series:
     >>> grb_pixs.withColumn("hpix", explode("hpix_circle"))\
           .orderBy("hpix")\
                .select(["triggerId", "hpix"]).head(5)
-    
+    [Row(triggerId=683499781, hpix=10), Row(triggerId=683499781, hpix=10), Row(triggerId=683499781, hpix=20), Row(triggerId=683499781, hpix=20), Row(triggerId=683499781, hpix=21)]
     """
     return pd.Series(
         [
