0512f0333661ea15b605181670d66fbacd75dff2
FusRoman <roman.lemontagner@gmail.com>
2023-04-20 08:50:13 +0000

    pep8 requirements

--- fink_grb/conftest.py
+++ fink_grb/conftest.py
@@ -233,19 +233,17 @@ def spatial_time_align(ztf_raw_data, grb_pdf):
     ztf_raw_data = ztf_raw_data.copy()
 
     all_obs = grb_pdf["raw_event"].map(get_observatory).values
-    random_obs = np.random.choice(all_obs, int( (len(all_obs) + 1) / 2 ))
+    random_obs = np.random.choice(all_obs, int((len(all_obs) + 1) / 2))
     jd_gcn = [obs.get_trigger_time()[1] for obs in random_obs]
-
 
     rand_ztf_index = np.random.choice(ztf_raw_data.index, len(random_obs))
     for rows_ztf_cand, rows_ztf_prv, new_jd in zip(
         ztf_raw_data.loc[rand_ztf_index, "candidate"],
         ztf_raw_data.loc[rand_ztf_index, "prv_candidates"],
-        jd_gcn
+        jd_gcn,
     ):
         rows_ztf_cand["jdstarthist"] = new_jd + np.random.uniform(1, 5)
         rows_ztf_prv[0]["jd"] = new_jd + np.random.uniform(0.1, 1)
-
 
     rand_ztf_index = np.random.choice(ztf_raw_data.index, len(random_obs))
     for rows_ztf, new_jd in zip(ztf_raw_data.loc[rand_ztf_index, "candidate"], jd_gcn):
@@ -253,8 +251,3 @@ def spatial_time_align(ztf_raw_data, grb_pdf):
         rows_ztf["prv_candidates"] = None
 
     return ztf_raw_data
-
-
-
-
-
