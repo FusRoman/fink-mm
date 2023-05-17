ee2006c8813563a2c54f8b68e8bc3d9abee2339d
FusRoman <roman.lemontagner@gmail.com>
2023-05-17 12:34:40 +0000

    update distribution schema

--- fink_grb/__init__.py
+++ fink_grb/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 __version__ = "0.6.1"
-__distribution_schema_version__ = "1.0"
+__distribution_schema_version__ = "1.1"
