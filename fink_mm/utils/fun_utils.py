4315fb78c92e5b8e1c34db25a885c8faf28a7ae5
Roman <roman.lemontagner@gmail.com>
2023-07-12 17:04:27 +0200

    test

--- fink_mm/utils/fun_utils.py
+++ fink_mm/utils/fun_utils.py
@@ -273,6 +273,9 @@ def get_observatory(obsname: str, rawEvent: str) -> Observatory:
 
     Example
     -------
+    >>> import os
+    >>> cwd = os.getcwd()
+    >>> print(cwd)
     >>> pdf = pd.read_parquet(grb_data)
     >>> type(get_observatory(pdf["observatory"].iloc[0], pdf["raw_event"].iloc[0]))
     <class 'Fermi.Fermi'>
