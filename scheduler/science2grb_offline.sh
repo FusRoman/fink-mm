54047359018c271a87cb2f236168bb9ab0eb2ffd
FusRoman <46221629+FusRoman@users.noreply.github.com>
2023-07-17 16:57:19 +0200

    Issue/80/refactoring into fink mm (#81)
    * start the refactoring
    
    * still refactoring
    
    * refactoring the last occurence of fink_grb
    
    * pep8 refactoring and requirements
    
    * refactoring workflows
    
    * restore fink_grb_bronze topics
    
    * test
    
    * rename fink-mm package
    
    * print grb_data
    
    * remove print
    
    * add gcn_test data
    
    * update gitignore
    
    * change columns name
    
    * update test data for distribution
    
    * fix get_assoc_proba test
    
    * pep8 requirements
    
    * update test data for online, test only online in ci
    
    * run all test
    
    * fink_added_value are part of the fink_mm output now
    
    * pep8 requirements
    
    * fix parquet reading issue
    
    * sort colums name before test
    
    * fix test I hope
    
    * test
    
    * add fink added value to the hbase column filter for the offline mode
    
    * new column filters for hbase
    
    * pep8
    
    * remove drop t2 in offline mode doctest
    
    * new test data for offline mode
    
    * -1.0 for offline data test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * pep8
    
    * test
    
    * drop t2 columns
    
    * drop t2 columns
    
    * run all tests
    
    * load gcn data of the previous day for the online mode
    
    * fix append in the online mode
    
    * print test
    
    * test
    
    * restore online as previous
    
    * all test
    
    * pep8

--- scheduler/science2grb_offline.sh
+++ scheduler/science2grb_offline.sh
@@ -3,12 +3,12 @@
 source ~/.bash_profile
 
 # Config 
-FINK_GRB_CONFIG="path/to/conf/fink_grb.conf"
-FINK_GRB_LOG="path/to/store/log"
+FINK_MM_CONFIG="path/to/conf/fink_mm.conf"
+FINK_MM_LOG="path/to/store/log"
 
 NIGHT=`date +"%Y%m%d" -d "now"`
 YEAR=${NIGHT:0:4}
 MONTH=${NIGHT:4:2}
 DAY=${NIGHT:6:2}
 
-nohup fink_grb join_stream offline --night=${NIGHT} --config ${FINK_GRB_CONFIG} > ${FINK_GRB_LOG}/fink_grb_offline_${YEAR}${MONTH}${DAY}.log &
+nohup fink_mm join_stream offline --night=${NIGHT} --config ${FINK_MM_CONFIG} > ${FINK_MM_LOG}/fink_mm_offline_${YEAR}${MONTH}${DAY}.log &
