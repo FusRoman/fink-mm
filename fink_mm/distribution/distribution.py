aa2cf5dbd1e7926942356e16774ee29a04b65d4f
Roman <roman.lemontagner@gmail.com>
2023-07-19 14:14:03 +0200

    drop mangrove

--- fink_mm/distribution/distribution.py
+++ fink_mm/distribution/distribution.py
@@ -94,6 +94,7 @@ def grb_distribution(
         .drop("day")
         .drop("timestamp")
         .drop("t2")
+        .drop("mangrove")
     )
 
     cnames = df_grb_stream.columns
@@ -104,7 +105,7 @@ def grb_distribution(
     ] = "cast(triggerTimeUTC as string) as triggerTimeUTC"
     cnames[cnames.index("lc_features_g")] = "struct(lc_features_g.*) as lc_features_g"
     cnames[cnames.index("lc_features_r")] = "struct(lc_features_r.*) as lc_features_r"
-    cnames[cnames.index("mangrove")] = "struct(mangrove.*) as mangrove"
+    # cnames[cnames.index("mangrove")] = "struct(mangrove.*) as mangrove"
     df_grb_stream = df_grb_stream.selectExpr(cnames)
 
     schema = schema_converter.to_avro(df_grb_stream.coalesce(1).limit(1).schema)
