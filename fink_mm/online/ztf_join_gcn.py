ddde70c679bde88883b41a7dc5d63802c8e8952f
Roman <roman.lemontagner@gmail.com>
2023-08-10 09:41:42 +0200

    broadcast gcn dataframe to the executors to do the join

--- fink_mm/online/ztf_join_gcn.py
+++ fink_mm/online/ztf_join_gcn.py
@@ -303,7 +303,7 @@ def ztf_join_gcn_stream(
         df_ztf_stream.hpix == df_grb_stream.hpix,
         df_ztf_stream.candidate.jdstarthist > df_grb_stream.triggerTimejd,
     ]
-    df_grb = df_ztf_stream.join(df_grb_stream, join_condition, "inner")
+    df_grb = df_ztf_stream.join(F.broadcast(df_grb_stream), join_condition, "inner")
 
     df_grb = join_post_process(df_grb)
 
