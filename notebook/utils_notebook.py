0dcef1078d265639a0b8bc68b67e2dbe39967ecb
FusRoman <roman.le-montagner@ijclab.in2p3.fr>
2023-09-07 13:42:50 +0200

    notebook works

--- notebook/utils_notebook.py
+++ notebook/utils_notebook.py
@@ -1,18 +1,15 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
-from fink_filters.filter_mm_module.filter import (
-    f_grb_bronze_events,
-    f_grb_silver_events,
-    f_grb_gold_events,
-)
+
+from fink_filters.filter_mm_module.filter import grb_bronze_events, grb_silver_events, grb_gold_events, gw_bronze_events
 
 def get_gold_and_silver(event_pdf):
     # bronze_mask = bronze_events(event_pdf["fink_class"], event_pdf["rb"])
-    silver_mask = f_grb_silver_events(
+    silver_mask = grb_silver_events(
         event_pdf["fink_class"], event_pdf["rb"], event_pdf["grb_proba"]
     )
-    gold_mask = f_grb_gold_events(
+    gold_mask = grb_gold_events(
         event_pdf["fink_class"], event_pdf["rb"], event_pdf["grb_proba"], event_pdf["rate"]
     )
 
@@ -20,30 +17,34 @@ def get_gold_and_silver(event_pdf):
     silver_pdf = event_pdf[~gold_mask & silver_mask]
     return gold_pdf, silver_pdf
 
-def plot_ztf_join_distribution(event_pdf, eventId):
+def plot_ztf_join_distribution(event_pdf, eventId, new_fink_mm=False):
+
+    cols = ["grb_ra", "grb_dec", "grb_proba", "grb_loc_error"]
+    if new_fink_mm:
+        cols = ["gcn_ra", "gcn_dec", "p_assoc", "gcn_loc_error"]
     def plot_grb_scatter(df, ax, marker, size, label):
         if len(df) == 0:
             return
-        deltaRAcosDEC = (df["ztf_ra"] - df["grb_ra"]) * np.cos(
+        deltaRAcosDEC = (df["ztf_ra"] - df[cols[0]]) * np.cos(
             np.radians(df["ztf_dec"])
         )
-        deltaDEC = np.abs((df["ztf_dec"] - df["grb_dec"]))
+        deltaDEC = np.abs((df["ztf_dec"] - df[cols[1]]))
 
         ax.scatter(
             deltaRAcosDEC, deltaDEC, alpha=0.5, s=size, marker=marker, label=label
         )
 
     def annotate_plot(df, ax):
-        deltaRAcosDEC = (df["ztf_ra"] - df["grb_ra"]) * np.cos(
+        deltaRAcosDEC = (df["ztf_ra"] - df[cols[0]]) * np.cos(
             np.radians(df["ztf_dec"])
         )
-        deltaDEC = np.abs((df["ztf_dec"] - df["grb_dec"]))
+        deltaDEC = np.abs((df["ztf_dec"] - df[cols[1]]))
 
         for dx, dy, objId in zip(deltaRAcosDEC, deltaDEC, df["objectId"]):
             ax.annotate(objId, xy=(dx, dy), xytext=(dx, dy), weight="bold")
 
     fink_class_event = event_pdf["fink_class"].unique()
-
+    print(f"fink class for this event: {fink_class_event}")
     fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=(15, 15))
     fig.suptitle(
         "ZTF alert distribution \nfor the GRB triggerId={}".format(eventId), fontsize=50
@@ -52,13 +53,13 @@ def plot_ztf_join_distribution(event_pdf, eventId):
     for _class in fink_class_event:
         tmp_pdf = event_pdf[event_pdf["fink_class"] == _class]
 
-        bronze_mask = f_grb_bronze_events(tmp_pdf["fink_class"], tmp_pdf["rb"])
-        silver_mask = f_grb_silver_events(
-            tmp_pdf["fink_class"], tmp_pdf["rb"], tmp_pdf["grb_proba"]
-        )
-        gold_mask = f_grb_gold_events(
-            tmp_pdf["fink_class"], tmp_pdf["rb"], tmp_pdf["grb_proba"], tmp_pdf["rate"]
-        )
+        bronze_args = [tmp_pdf["fink_class"], tmp_pdf["observatory"], tmp_pdf["rb"]]
+        silver_args = bronze_args + [tmp_pdf[cols[2]]]
+        gold_args = bronze_args + [tmp_pdf[cols[3]], tmp_pdf[cols[2]], tmp_pdf["rate"]]
+
+        bronze_mask = grb_bronze_events(*bronze_args)
+        silver_mask = grb_silver_events(*silver_args)
+        gold_mask = grb_gold_events(*gold_args)
 
         gold_pdf = tmp_pdf[gold_mask]
         silver_pdf = tmp_pdf[~gold_mask & silver_mask]
@@ -96,7 +97,7 @@ def plot_ztf_join_distribution(event_pdf, eventId):
 
     ax.tick_params(axis="x", which="major", labelsize=30)
     ax.tick_params(axis="y", which="major", labelsize=25)
-    ax.legend(prop={"size": 35})
+    ax.legend(prop={"size": 15})
     plt.tight_layout()
 
     plt.show()
